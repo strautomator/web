3105a9fa0574c3cc863c2ad48a2ce0bb6a548766
Igor Ramadas <igor@devv.com>
2020-04-19 01:35:44 +0200

    The checkSubscriptions() is now part of the Strava core.

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -1,6 +1,6 @@
 // Strautomator: Scheduler
 
-import {recipes, strava, users, RecipeData, StravaWebhook, UserData} from "strautomator-core"
+import {recipes, strava, users, RecipeData, UserData} from "strautomator-core"
 import _ = require("lodash")
 import logger = require("anyhow")
 const settings = require("setmeup").settings
@@ -14,11 +14,6 @@ class Scheduler {
     static get Instance(): Scheduler {
         return this._instance || (this._instance = new this())
     }
-
-    /**
-     * Timer to check and reset subscriptions (webhooks) on Strava.
-     */
-    private timerSubscriptions = null
 
     /**
      * Timer to retry failed activity updates every few minutes.
@@ -37,11 +32,7 @@ class Scheduler {
      * Init the scheduler. Failed jobs will be checked once every minute.
      */
     init = async (): Promise<void> => {
-        this.timerSubscriptions = setInterval(this.checkSubscriptions, 1000 * 60 * 60 * 24)
         this.timerRetry = setInterval(this.retryJobs, 1000 * 60)
-
-        // Check Strava subscriptions with a 1 minute delay.
-        setTimeout(this.checkSubscriptions, 60000)
     }
 
     /**
@@ -49,57 +40,10 @@ class Scheduler {
      */
     shutdown = async (): Promise<void> => {
         try {
-            clearInterval(this.timerSubscriptions)
             clearInterval(this.timerRetry)
             await this.retryJobs()
         } catch (ex) {
             logger.error("Scheduler.shutdown", ex)
-        }
-    }
-
-    // STRAVA SUBSCRIPTIONS
-    // --------------------------------------------------------------------------
-
-    /**
-     * Periodically check user subscriptions and renew them, if needed.
-     */
-    checkSubscriptions = async (): Promise<void> => {
-        try {
-            const subscriptions = await strava.getSubscriptions()
-            const idleUsers = await users.getIdle()
-
-            logger.info("Scheduler.checkSubscriptions", `${subscriptions.length} webhooks`, `${idleUsers.length} idle users`)
-
-            // Iterate users and make sure subscriptions are active.
-            for (let user of idleUsers) {
-                try {
-                    let sub: StravaWebhook
-
-                    // Find correct subscription for the user.
-                    for (let s of subscriptions) {
-                        if (s.id == user.stravaSubscription) {
-                            sub = s
-                            break
-                        } else if (s.callbackUrl.indexOf(`${settings.strava.api.urlToken}/${user.id}`) > 0) {
-                            logger.warn("Scheduler.checkSubscriptions", `User ${user.id} - ${user.displayName}`, `Wrong subscription ID ${user.stravaSubscription}, correct is ${s.id}`)
-                            sub = s
-                            break
-                        }
-                    }
-
-                    // Reset subscription if it's not valid.
-                    if (!sub) {
-                        logger.info("Scheduler.checkSubscriptions", `User ${user.id} - ${user.displayName}`, `Subscription ${user.stravaSubscription || "empty"}`, `Needs renewal`)
-                        await strava.setSubscription(user)
-                    } else {
-                        logger.debug("Scheduler.checkSubscriptions", `User ${user.id} - ${user.displayName}`, `Subscription ${user.stravaSubscription} is active`)
-                    }
-                } catch (ex) {
-                    logger.debug("Sccheduler.checkSubscriptions", `User ${user.id} - ${user.displayName}`, user.stravaSubscription, "Failed", ex)
-                }
-            }
-        } catch (ex) {
-            logger.error("Scheduler.checkSubscriptions", ex)
         }
     }
 
