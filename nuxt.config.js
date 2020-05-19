20c76d68ad3a700b7a59b8b573e1c155bf22af77
Igor Ramadas <igor@devv.com>
2020-05-19 23:55:27 +0200

    Updated colours and dependencies.

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,7 +82,7 @@ module.exports = {
                     info: colors.grey.lighten1,
                     warning: colors.amber.base,
                     error: colors.deepOrange.accent4,
-                    success: colors.green.accent3
+                    success: colors.lightGreen.darken4
                 }
             }
         }
