22cb28c7e65d4dc85d1ccf21e2cc5685b258c3d8
Igor Ramadas <igor@devv.com>
2020-07-03 23:07:00 +0200

    Improved webError() handling of error messages.

--- plugins/errorhandler.js
+++ plugins/errorhandler.js
@@ -2,10 +2,18 @@ import Vue from "vue"
 
 Vue.prototype.$webError = async (method, ex) => {
     try {
-        const status = ex.status || ex.statusCode || 500
-        const message = ex.message || ex.toString()
-        const title = ex.title || ""
+        const responseData = ex.response ? ex.response.data : null
+        let status = ex.status || ex.statusCode || 500
 
+        // Get error message.
+        let message = responseData ? responseData.message : ex.message
+        if (!message) message = ex.toString()
+
+        // Get error title.
+        let title = responseData ? responseData.reason : ex.title
+        if (!title) title = ""
+
+        // Running on the server or the client?
         if (process.server) {
             const logger = require("anyhow")
             logger.error("Vue", method, `Status ${status}`, `${title || "Error"} - ${message}`)
