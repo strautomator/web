7486008f40650bd79feb6b4b5d02f6e781eae4be
Igor Ramadas <igor@devv.com>
2020-07-05 15:14:29 +0200

    Make sure user information is updated at least once every 10 minutes.

--- mixins/userMixin.js
+++ mixins/userMixin.js
@@ -10,9 +10,19 @@ export default {
             return !this.user.isPro && Object.keys(this.user.recipes).length >= this.$store.state.freePlanDetails.maxRecipes
         }
     },
-    mounted() {
+    async fetch() {
         if (this.$axios && this.$store.state.oauth) {
             this.$axios.setToken(this.$store.state.oauth.accessToken)
+
+            const minTimestamp = new Date().valueOf() - 600000
+
+            // Make sure current user information is never older than 10 minutes.
+            if (!this.$store.state.lastUserFetch || this.$store.state.lastUserFetch < minTimestamp) {
+                const user = await this.$axios.$get(`/api/users/${this.user.id}?refresh=1`)
+                this.$store.commit("setLastUserFetch", new Date().valueOf())
+                this.$store.commit("setUser", user)
+                this.user = user
+            }
         }
     }
 }
