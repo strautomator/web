a84fa9815647bb592a1749e2ef279496870625cc
Igor Ramadas <igor@devv.com>
2020-07-10 16:24:09 +0200

    Moved help page to a subdirectory.

---
+++ pages/help/tutorial.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-layout column>
+        <h1>Tutorial</h1>
+        <p>Coming soon...</p>
+    </v-layout>
+</template>
+
+<script>
+export default {
+    authenticated: false,
+    layout({store}) {
+        if (!store.state.oauth || !store.state.user) {
+            return "landing"
+        } else {
+            return "default"
+        }
+    },
+    head() {
+        return {
+            title: "Tutorial"
+        }
+    }
+}
+</script>
