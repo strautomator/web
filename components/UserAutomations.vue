e72e73ecde76053cb3fd6147ebedbd408b9b4b23
Igor Ramadas <igor@devv.com>
2020-11-15 16:00:19 +0100

    Adding drag-n-drop reordering support on the Automations view #1.

--- components/UserAutomations.vue
+++ components/UserAutomations.vue
@@ -1,46 +1,41 @@
 <template>
     <div>
-        <v-card class="mb-5" v-for="recipe in recipes" :key="recipe.id" outlined>
-            <v-hover v-slot:default="{hover}">
-                <n-link :to="'/automations/edit?id=' + recipe.id" :title="recipe.title">
-                    <v-card-title class="accent">
-                        <v-icon class="ml-n1 mr-2" color="primary" v-if="recipe.defaultFor">{{ getSportIcon(recipe.defaultFor) }}</v-icon>
-                        <span class="primary--text">{{ recipe.title }}</span>
-                        <v-spacer />
-                        <v-icon v-show="hover" small>mdi-pencil-outline</v-icon>
-                    </v-card-title>
-                </n-link>
-            </v-hover>
-            <v-card-text class="white--text pb-1 pb-md-2">
-                <ul class="mt-0 pl-4 condition-list">
-                    <li v-if="recipe.defaultFor">Default automation for all "{{ getSportName(recipe.defaultFor) }}" activities</li>
-                    <li v-for="(condition, index) in recipe.conditions" :key="`condition-${index}`">
-                        {{ conditionSummary(condition) }}
-                    </li>
-                </ul>
-                <ul class="mt-1 pl-4 action-list">
-                    <li class="font-weight-medium" v-for="(action, index) in recipe.actions" :key="`action-${index}`">
-                        {{ actionSummary(action) }}
-                    </li>
-                </ul>
-                <div class="mt-2 float-left" v-if="recipeStats[recipe.id]">
-                    <v-chip class="mb-0 ml-1" disabled outlined small>Executed {{ recipeStats[recipe.id].activities.length }} times, last: {{ recipeStats[recipe.id].dateLastTrigger }}</v-chip>
-                </div>
-                <div class="text-right mr-n1 nowrap">
-                    <template v-if="recipe.defaultFor">
-                        <v-chip class="text-lowercase" small outlined>default for {{ getSportName(recipe.defaultFor) }}</v-chip>
-                    </template>
-                    <template v-else>
-                        <v-btn color="grey lighten-1" title="Send this automation down (executes last)" class="mr-1" @click="setRecipeOrder(recipe, 1)" icon>
-                            <v-icon>mdi-arrow-down-circle</v-icon>
-                        </v-btn>
-                        <v-btn color="grey lighten-1" title="Send this automation up (executes first)" class="mr-n1" @click="setRecipeOrder(recipe, -1)" icon>
-                            <v-icon>mdi-arrow-up-circle</v-icon>
-                        </v-btn>
-                    </template>
-                </div>
-            </v-card-text>
-        </v-card>
+        <draggable v-model="recipes" handle=".drag-handle" draggable=".sortablerecipe" v-bind="dragOptions" @change="recipeReordered" @start="dragging = true" @end="dragging = false">
+            <transition-group type="transition" :name="!dragging ? 'flip-list' : null">
+                <v-card class="mb-5" :class="{sortablerecipe: !recipe.defaultFor}" v-for="recipe in recipes" :key="recipe.id" outlined>
+                    <v-hover v-slot:default="{hover}">
+                        <n-link :to="'/automations/edit?id=' + recipe.id" :title="recipe.title">
+                            <v-card-title class="accent">
+                                <v-icon class="ml-n1 mr-2" color="primary" v-if="recipe.defaultFor">{{ getSportIcon(recipe.defaultFor) }}</v-icon>
+                                <span class="primary--text">{{ recipe.title }}</span>
+                                <v-spacer />
+                                <v-icon v-show="hover" small>mdi-pencil-outline</v-icon>
+                                <v-icon class="drag-handle ml-1" title="Hold to reorded the automation recipe" v-if="!recipe.defaultFor">mdi-drag</v-icon>
+                            </v-card-title>
+                        </n-link>
+                    </v-hover>
+                    <v-card-text class="white--text pb-1 pb-md-2">
+                        <ul class="mt-0 pl-4 condition-list">
+                            <li v-if="recipe.defaultFor">Default automation for all "{{ getSportName(recipe.defaultFor) }}" activities</li>
+                            <li v-for="(condition, index) in recipe.conditions" :key="`condition-${index}`">
+                                {{ conditionSummary(condition) }}
+                            </li>
+                        </ul>
+                        <ul class="mt-1 pl-4 action-list">
+                            <li class="font-weight-medium" v-for="(action, index) in recipe.actions" :key="`action-${index}`">
+                                {{ actionSummary(action) }}
+                            </li>
+                        </ul>
+                        <div class="mt-2" v-if="recipeStats[recipe.id]">
+                            <v-chip class="mb-0 ml-1" disabled outlined small>Executed {{ recipeStats[recipe.id].activities.length }} times, last: {{ recipeStats[recipe.id].dateLastTrigger }}</v-chip>
+                        </div>
+                        <div class="text-right mr-n1 nowrap">
+                            <v-chip class="text-lowercase" v-if="recipe.defaultFor" small outlined>default for {{ getSportName(recipe.defaultFor) }}</v-chip>
+                        </div>
+                    </v-card-text>
+                </v-card>
+            </transition-group>
+        </draggable>
         <div class="mt-5 text-center text-md-left">
             <v-btn v-if="!needsProRecipes" color="primary" to="/automations/edit" title="Create a new automation" rounded nuxt>
                 <v-icon left>mdi-plus-circle</v-icon>
@@ -70,27 +65,51 @@
 .condition-list {
     list-style-type: circle;
 }
+.drag-handle {
+    cursor: move;
+}
+.drag-ghost {
+    opacity: 0.2;
+}
+.flip-list-move {
+    transition: transform 0.5s;
+}
+.no-move {
+    transition: transform 0s;
+}
 </style>
 
 <script>
 import _ from "lodash"
+import draggable from "vuedraggable"
 import userMixin from "~/mixins/userMixin.js"
 import recipeMixin from "~/mixins/recipeMixin.js"
 
 export default {
     authenticated: true,
     mixins: [userMixin, recipeMixin],
+    components: {draggable},
     data() {
         return {
             hasChanges: false,
+            dragging: false,
             recipeStats: {},
             recipes: []
+        }
+    },
+    computed: {
+        dragOptions() {
+            return {
+                animation: 200,
+                ghostClass: "drag-ghost"
+            }
         }
     },
     async fetch() {
         try {
             const recipeStats = {}
             const arrStats = await this.$axios.$get(`/api/users/${this.user.id}/recipes/stats`)
+
             for (let stats of arrStats) {
                 const recipeId = stats.id.split("-")[1]
                 stats.dateLastTrigger = this.$moment(stats.dateLastTrigger).format("ll")
@@ -106,30 +125,15 @@ export default {
         this.delaySaveOrder = _.debounce(this.saveOrder, 4000)
     },
     mounted() {
-        this.reorderRecipes(_.cloneDeep(Object.values(this.user.recipes)))
+        this.setOrderedRecipes(_.cloneDeep(Object.values(this.user.recipes)))
     },
     methods: {
-        reorderRecipes(recipes) {
+        setOrderedRecipes(recipes) {
             if (!recipes) recipes = this.recipes
             this.recipes = _.sortBy(recipes, ["defaultFor", "order", "title"])
         },
-        setRecipeOrder(recipe, offset) {
-            const index = _.indexOf(this.recipes, recipe)
-            const neighbour = this.recipes[index + offset]
-
-            // Make sure ordering will only change inside the bounds.
-            if (neighbour && !neighbour.defaultFor) {
-                if (offset < 0 && neighbour.order >= 0) {
-                    this.recipes[index + offset].order++
-                    recipe.order--
-                } else if (offset > 0) {
-                    this.recipes[index + offset].order--
-                    recipe.order++
-                }
-            }
-
+        recipeReordered() {
             this.hasChanges = true
-            this.reorderRecipes()
             this.delaySaveOrder()
         },
         async saveOrder() {
@@ -137,11 +141,14 @@ export default {
             this.hasChanges = false
 
             try {
-                const data = {}
+                let index = 0
+                let data = {}
 
                 // Create object to update the order of recipes.
                 for (let recipe of this.recipes) {
-                    data[recipe.id] = recipe.order
+                    index++
+                    recipe.order = index
+                    data[recipe.id] = index
                     this.$store.commit("setUserRecipe", _.cloneDeep(recipe))
                 }
 
