3af14aef151b0bfa0c3b2ad3ce7f42c011858e6e
Igor Ramadas <igor@devv.com>
2021-01-06 17:46:36 +0100

    Small note about the dashboard not displaying activities that were not updated by Strautomator.

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -85,6 +85,9 @@
                                 </tr>
                             </tbody>
                         </v-simple-table>
+                        <div class="caption mt-5 ml-5">
+                            Please note that the list above does not show activities that were not updated by Strautomator.
+                        </div>
                     </v-card-text>
                 </v-card>
                 <v-alert class="mt-4 text-center text-md-left">
