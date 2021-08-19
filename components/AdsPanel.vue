620d15d8204fde0f0353490fe8ed34da9105fdd6
Igor Ramadas <igor@devv.com>
2021-08-19 22:30:30 +0200

    Initial version of the ads panel.

---
+++ components/AdsPanel.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-card class="affiliates-card mt-6" v-if="!hidden" outlined>
+        <v-card-text class="white">
+            <div class="mb-4 white black--text text-center text-h6">Looking for some new gear?</div>
+            <v-row>
+                <v-col cols="4">
+                    <a :href="linkAmazon" target="amazon"><v-img src="/images/affiliates/amazon.png" max-height="100px"/></a>
+                </v-col>
+                <v-col cols="4">
+                    <a :href="linkWiggle" target="wiggle"><v-img src="/images/affiliates/wiggle.png" max-height="100px"/></a>
+                </v-col>
+                <v-col cols="4">
+                    <a :href="linkIcan" target="ican"><v-img src="/images/affiliates/ican.png" max-height="100px"/></a>
+                </v-col>
+            </v-row>
+            <div class="mt-4 caption black--text text-center">
+                Using these affiliate stores is a win-win: you get the best deals, and Strautomator gets a small comission to keep the servers running.
+            </div>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data() {
+        let hidden = false
+        let tagAmazon = "sports"
+
+        try {
+            const bikeCount = (this.$store.state.user.profile.bikes || []).length
+            const shoeCount = (this.$store.state.user.profile.shoes || []).length
+
+            if (bikeCount > shoeCount) tagAmazon = "cycling"
+            else if (shoeCount > bikeCount) tagAmazon = "running"
+
+            hidden = false && this.$store.state.user && this.$store.state.user.isPro
+        } catch (ex) {
+            console.error(ex)
+        }
+
+        return {
+            hidden: hidden,
+            linkAmazon: `https://links.devv.com/l/amazon-${tagAmazon}`,
+            linkIcan: `https://links.devv.com/l/ican`,
+            linkWiggle: `https://links.devv.com/l/wiggle`
+        }
+    }
+}
+</script>
