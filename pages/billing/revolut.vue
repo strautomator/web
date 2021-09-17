684d2d46109fba76d19218e2f96b8e46eb40bbb3
Igor Ramadas <igor@devv.com>
2021-09-17 12:02:47 +0200

    Updated Revolut campaign details.

--- pages/billing/revolut.vue
+++ pages/billing/revolut.vue
@@ -1,6 +1,20 @@
 <template>
     <v-layout column>
-        <v-container v-if="available" fluid>
+        <v-container v-if="subscriptionSource == 'revolut'" fluid>
+            <v-card>
+                <v-card-text class="text-center">
+                    <p>Thanks for participating! You already have a lifetime PRO account ☺</p>
+                    <img src="/images/affiliates/revolut-logo.png" width="64" height="64" class="mt-2" />
+                </v-card-text>
+            </v-card>
+            <div class="mt-6 text-center">
+                <v-btn color="primary" to="/account" rounded nuxt>
+                    <v-icon left>mdi-arrow-left</v-icon>
+                    Back to my account
+                </v-btn>
+            </div>
+        </v-container>
+        <v-container v-else-if="available" fluid>
             <div class="mt-4 mb-8 text-center display-2 font-weight-black">Lifetime PRO, with Revolut!</div>
             <v-card>
                 <v-card-text>
@@ -13,15 +27,18 @@
                         To be eligible, you'll need to complete these steps by <strong>{{ $dayjs(dateEnd).format("lll") }}</strong>
                     </p>
                     <ul class="ml-n2">
-                        <li>Sign up to Revolut and verify your ID</li>
+                        <li>Sign up to Revolut with our link</li>
+                        <li>Verify your identity with Revolut</li>
                         <li>Add money to your new account</li>
                         <li>Request a physical Revolut card</li>
                         <li>Complete at least 3 purchases with your new physical or a virtual card</li>
                     </ul>
                     <p class="mt-3">
-                        If you sign up using the same email you have registered in Strautomator, there are no extra steps, and your Strautomator account will be upgraded to PRO automatically. Otherwise please send your full name or email to
-                        <a href="mailto:info@strautomator.com">info@strautomator.com</a>, and I'll manually active your PRO subscription within 48 hours.
+                        If you sign up using the same email you have registered in Strautomator, the upgrade to PRO should happen automatically after a few days. Otherwise please send your full name or email to
+                        <a href="mailto:info@strautomator.com">info@strautomator.com</a>, and I'll manually active your PRO subscription.
                     </p>
+                    <p>Eligible users who complete the steps will have their lifetime PRO activated by {{ $dayjs(dateSwitch).format("ll") }} at the latest.</p>
+
                     <div class="mt-5 text-center text-md-left">
                         <a href="https://links.devv.com/l/revolut" title="Sign up to Revolut" target="revolut"
                             ><v-btn color="primary" rounded nuxt>
@@ -30,13 +47,28 @@
                             </v-btn></a
                         >
                     </div>
+
+                    <v-alert class="mt-6 text-caption" color="accent">
+                        <div class="mb-4" v-if="subscriptionSource == 'paypal'">
+                            You already have a PRO subscription via PayPal.
+                            <br v-if="$breakpoint.mdAndUp" />
+                            If you participate in this campaign, your PayPal subscription will be cancelled so no future payments are needed.
+                        </div>
+                        Please note that Revolut has no direct affiliation to Strautomator!
+                        <br v-if="$breakpoint.mdAndUp" />
+                        For more details about the "Lifetime PRO with Revolut" campaign, please check the <n-link to="/help?q=revolut" nuxt>help</n-link> section.
+                        <br />
+                        Best regards,
+                        <br />
+                        Igor
+                    </v-alert>
                 </v-card-text>
             </v-card>
         </v-container>
         <v-container v-else fluid>
             <v-card>
                 <v-card-text>
-                    <p>The "Lifetime PRO, with Revolut" campaign is currently not running. But of course, you can still <a href="https://links.devv.com/l/revolut">open a Revolut account</a> if you wish to :-)</p>
+                    <p class="text-center">The "Lifetime PRO with Revolut" campaign is currently not running. If you want a PRO account, please subscribe via PayPal or GitHub.</p>
                 </v-card-text>
             </v-card>
             <div class="mt-6 text-center">
@@ -69,16 +101,23 @@ export default {
             dateSwitch: null
         }
     },
+    computed: {
+        subscriptionSource() {
+            if (this.user.isPro && this.user.subscription) {
+                return this.user.subscription.source
+            }
+            return null
+        }
+    },
     async fetch() {
         try {
             const announcements = await this.$axios.$get("/api/announcements/active")
 
             for (let a of announcements) {
-                console.dir(a)
                 if (a.id == "revolut-pro") {
                     this.available = true
                     this.dateEnd = a.dateExpiry
-                    this.dateSwitch = this.$dayjs(a.dateExpiry).add(2, "days")
+                    this.dateSwitch = this.$dayjs(a.dateExpiry).add(9, "days")
                 }
             }
         } catch (ex) {
