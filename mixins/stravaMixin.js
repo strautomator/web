6f351bd812485e2823207c9fcedd77c4d206a558
Igor Ramadas <igor@devv.com>
2021-11-07 14:06:12 +0100

    Improved detection of Strava outages, now considering any major or critical incident as an outage.

--- mixins/stravaMixin.js
+++ mixins/stravaMixin.js
@@ -11,20 +11,12 @@ export default {
             try {
                 const res = await fetch("https://status.strava.com/api/v2/summary.json")
                 const data = await res.json()
-                const issueKeywords = ["garmin", "uploads", "site degraded", "strava degraded", "site outage", "site slowness", "site instability"]
 
                 if (data.incidents && data.incidents.length > 0) {
                     const incident = _.find(data.incidents, (i) => i.impact.toLowerCase() == "critical" || i.impact.toLowerCase() == "major")
-                    if (!incident) return
+                    if (!incident || incident.resolved_at) return
 
-                    const description = incident.name.toLowerCase()
-
-                    for (let keyword of issueKeywords) {
-                        if (description.indexOf(keyword) >= 0) {
-                            this.stravaStatus = incident.name
-                            return
-                        }
-                    }
+                    this.stravaStatus = incident.name
                 }
             } catch (ex) {
                 console.error("Could not get API status from Strava", ex)
