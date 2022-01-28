d50900cc3bd73d386c9b47716d477e624bcf4841
Igor Ramadas <igor@devv.com>
2022-01-28 20:26:29 +0100

    Force hide ads for some days, till the new affiliate links are sorted out.

--- components/AdsPanel.vue
+++ components/AdsPanel.vue
@@ -3,19 +3,11 @@
         <v-card-text class="white">
             <div class="mb-5 white black--text text-center text-h6">Looking for some new gear?</div>
             <v-row>
-                <v-col cols="4">
-                    <a :href="linkAmazon" target="amazon" title="Sports gear @ Amazon"><v-img src="/images/affiliates/amazon.png" max-height="100px"/></a>
-                </v-col>
-                <v-col cols="4">
-                    <a :href="linkWiggle" target="wiggle" title="Sports gear @ Wiggle"><v-img src="/images/affiliates/wiggle.png" max-height="100px"/></a>
-                </v-col>
-                <v-col cols="4">
-                    <a :href="linkIcan" target="ican" title="Carbon wheels and frames @ ICAN"><v-img src="/images/affiliates/ican.png" max-height="100px"/></a>
+                <v-col cols="6">
+                    <a :href="linkIcan" target="ican" title="Carbon wheels and frames @ ICAN"><v-img src="/images/affiliates/ican.png" max-height="100px" /></a>
                 </v-col>
             </v-row>
-            <div class="mt-4 caption black--text text-center">
-                Using these affiliate stores is a win-win: you get the best deals, and Strautomator gets a small comission to keep the servers running.
-            </div>
+            <div class="mt-4 caption black--text text-center">Using our affiliate stores is a win-win: you get the best deals, and Strautomator gets a small comission to keep the servers running.</div>
         </v-card-text>
     </v-card>
 </template>
@@ -38,14 +30,13 @@ export default {
         }
 
         return {
-            linkAmazon: `https://links.devv.com/l/amazon-${tagAmazon}`,
-            linkIcan: `https://links.devv.com/l/ican`,
-            linkWiggle: `https://links.devv.com/l/wiggle`
+            linkIcan: `https://links.devv.com/l/ican`
         }
     },
     computed: {
         hidden() {
-            return this.$store.state.user && this.$store.state.user.isPro && this.proHide
+            return true
+            //return this.$store.state.user && this.$store.state.user.isPro && this.proHide
         }
     }
 }
