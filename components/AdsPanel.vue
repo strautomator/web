4f4b42a419b609bdc9cab97e4aa56e5178f315ae
Igor Ramadas <igor@devv.com>
2022-06-07 15:00:27 +0200

    Reactivated the ads panel (for free users only).

--- components/AdsPanel.vue
+++ components/AdsPanel.vue
@@ -1,10 +1,16 @@
 <template>
     <v-card class="affiliates-card mt-12" v-if="!hidden" outlined>
-        <v-card-text class="white">
-            <div class="mb-5 white black--text text-center text-h6">Looking for some new gear?</div>
+        <v-card-text class="grey lighten-2">
+            <div class="mb-5 grey lighten-2 black--text text-center text-h6">Affiliates</div>
             <v-row>
-                <v-col cols="6">
-                    <a :href="linkIcan" target="ican" title="Carbon wheels and frames @ ICAN"><v-img src="/images/affiliates/ican.png" max-height="100px" /></a>
+                <v-col cols="4">
+                    <a :href="linkIcan" target="ican" title="Carbon wheels and frames @ ICAN"><v-img src="/images/affiliates/ican.png" max-height="58px" /></a>
+                </v-col>
+                <v-col cols="4">
+                    <a :href="linkProBikeKit" target="probikekit" title="Cycling and sports gear @ ProBikeKit"><v-img src="/images/affiliates/probikekit.png" max-height="58px" /></a>
+                </v-col>
+                <v-col cols="4">
+                    <a :href="linkNextDns" target="probikekit" title="The new firewall for the modern Internet"><v-img src="/images/affiliates/nextdns.png" max-height="58px" /></a>
                 </v-col>
             </v-row>
             <div class="mt-4 caption black--text text-center">Using our affiliate stores is a win-win: you get the best deals, and Strautomator gets a small comission to keep the servers running.</div>
@@ -14,29 +20,31 @@
 
 <script>
 export default {
-    props: ["pro-hide"],
     data() {
-        let tagAmazon = "sports"
+        let sportsTag = "sports"
 
         try {
             const user = this.$store.state.user || {profile: {}}
             const bikeCount = (user.profile.bikes || []).length
             const shoeCount = (user.profile.shoes || []).length
 
-            if (bikeCount > shoeCount) tagAmazon = "cycling"
-            else if (shoeCount > bikeCount) tagAmazon = "running"
+            if (bikeCount > shoeCount) sportsTag = "cycling"
+            else if (shoeCount > bikeCount) sportsTag = "running"
         } catch (ex) {
             console.error(ex)
         }
 
         return {
-            linkIcan: `https://links.devv.com/l/ican`
+            linkBitpanda: "https://links.devv.com/l/bitpanda",
+            linkIcan: "https://links.devv.com/l/ican",
+            linkNextDns: "https://links.devv.com/l/nextdns",
+            linkProBikeKit: "https://links.devv.com/l/probikekit",
+            linkRevolut: "https://links.devv.com/l/revolut"
         }
     },
     computed: {
         hidden() {
-            return true
-            //return this.$store.state.user && this.$store.state.user.isPro && this.proHide
+            return this.$store.state.user && this.$store.state.user.isPro
         }
     }
 }
