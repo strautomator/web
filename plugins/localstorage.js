26b36ec41980c3c45f80afd55be3c14c90b235f6
Igor Ramadas <igor@devv.com>
2022-08-06 22:33:55 +0300

    New $localstorage plugin.

---
+++ plugins/localstorage.js
@@ -0,0 +1,35 @@
+import Vue from "vue"
+
+Vue.prototype.$getLocalStorage = (key) => {
+    try {
+        const itemJson = window.localStorage.getItem(key)
+        if (!itemJson) {
+            return null
+        }
+
+        const item = JSON.parse(itemJson)
+        if (!item || (item.expires && item.expires < Math.round(new Date().valueOf() / 1000))) {
+            return null
+        }
+
+        return item.data
+    } catch (ex) {
+        console.error("Vue.getLocalStorage", key, ex)
+        return null
+    }
+}
+
+Vue.prototype.$setLocalStorage = (key, data, maxAgeSeconds) => {
+    try {
+        const item = {data: data}
+
+        // Optional age to set the expiration timestamp (as seconds).
+        if (maxAgeSeconds) {
+            item.expires = Math.round(new Date().valueOf() / 1000) + maxAgeSeconds
+        }
+
+        window.localStorage.setItem(key, JSON.stringify(item, null, 0))
+    } catch (ex) {
+        console.error("Vue.setLocalStorage", key, ex)
+    }
+}
