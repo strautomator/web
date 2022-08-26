b2f471be5f8545f34b7abb82819a2c64e48be9e9
Igor Ramadas <igor@devv.com>
2022-08-26 21:20:21 +0200

    New weather endpoint to get multiple forecasts for upcoming events.

--- src/routes/api/weather.ts
+++ src/routes/api/weather.ts
@@ -1,16 +1,26 @@
 // Strautomator API: Weather
 
-import {weather, UserData} from "strautomator-core"
+import {weather, UserData, WeatherSummary} from "strautomator-core"
 import auth from "../auth"
 import express = require("express")
 import logger = require("anyhow")
 import webserver = require("../../webserver")
+import dayjs from "dayjs"
+const settings = require("setmeup").settings
 const router = express.Router()
+
+// Multi forecast result.
+interface MultiForecastResult {
+    coordinates?: [number, number]
+    timestamp?: number
+    forecast?: WeatherSummary
+    error?: Error
+}
 
 /**
  * Weather for the specified coordinates.
  */
-router.get("/:userId/:coordinates", async (req: express.Request, res: express.Response) => {
+router.get("/:userId/coordinates/:coordinates", async (req: express.Request, res: express.Response) => {
     try {
         if (!req.params) throw new Error("Missing request params")
 
@@ -35,7 +45,59 @@ router.get("/:userId/:coordinates", async (req: express.Request, res: express.Re
         }
 
         logger.info("Routes", req.method, req.originalUrl)
-        res.set("Cache-Control", "public, max-age=60")
+        res.set("Cache-Control", "public, max-age=300")
+        webserver.renderJson(req, res, result)
+    } catch (ex) {
+        logger.error("Routes", req.method, req.originalUrl, ex)
+        res.status(500).json({error: ex.toString()})
+    }
+})
+
+/**
+ * Weather forecast for the specified locations and dates, using the "data" query with the
+ * following format: "id:lat,long:timestamp|id:lat,long:timestamp|id:lat,long:timestamp|[...]"
+ */
+router.get("/:userId/multi-forecast", async (req: express.Request, res: express.Response) => {
+    try {
+        if (!req.params) throw new Error("Missing request params")
+
+        const user: UserData = (await auth.requestValidator(req, res, {referer: true})) as UserData
+        if (!user) return
+
+        if (!req.query.data) {
+            logger.error("Routes", req.method, req.originalUrl, "Missing request query")
+            return webserver.renderError(req, res, "Missing request query", 400)
+        }
+
+        const arrQuery = req.query.data.toString().split("|")
+        const result: MultiForecastResult[] = []
+        let hadError = false
+
+        // Helper to get a single forecast.
+        const getForecast = async (query): Promise<void> => {
+            let queryResult: MultiForecastResult = {}
+            try {
+                const arrData = query.split(":")
+                queryResult.coordinates = arrData[1].split(",").map((c) => parseFloat(c)) as [number, number]
+                queryResult.timestamp = parseInt(arrData[2])
+                queryResult.forecast = await weather.getLocationWeather(user, queryResult.coordinates, dayjs.unix(queryResult.timestamp).utc().toDate())
+            } catch (weatherEx) {
+                logger.warn("Routes", req.method, req.path, query, weatherEx.message)
+                hadError = true
+                queryResult.error = weatherEx
+            } finally {
+                result.push(queryResult)
+            }
+        }
+
+        // Get forecasts in parallel.
+        const batchSize = user.isPro ? settings.plans.pro.apiConcurrency : settings.plans.free.apiConcurrency
+        while (arrQuery.length) {
+            await Promise.all(arrQuery.splice(0, batchSize).map(getForecast))
+        }
+
+        logger.info("Routes", req.method, req.originalUrl)
+        res.set("Cache-Control", `public, max-age=${hadError ? "60" : "600"}`)
         webserver.renderJson(req, res, result)
     } catch (ex) {
         logger.error("Routes", req.method, req.originalUrl, ex)
