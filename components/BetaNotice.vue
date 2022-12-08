3dfa64b645849e369f001e2c17c122801474eff0
Igor Ramadas <igor@devv.com>
2022-12-08 21:40:19 +0100

    Beta panel with notes about the beta environment.

---
+++ components/BetaNotice.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-card v-if="$store.state.beta" class="mb-4" outlined>
+        <v-card-text class="pa-4 text-left">
+            <ul class="pl-3">
+                <li>Only available to PRO users.</li>
+                <li>Not possible (and not actually needed) to subscribe to PRO via PayPal.</li>
+                <li>Beta profiles and configurations are independent and will not affect the production accounts.</li>
+                <li>All user-related data will be deleted after 3 months, except for the user account itself.</li>
+                <li>The Spotify integration is restricted, please get in touch if you want to use it in Beta.</li>
+                <li>GearWear calculations and notifications are not guaranteed to work.</li>
+                <li>The FTP auto update feature is disabled.</li>
+                <li>Expect things to break!</li>
+            </ul>
+        </v-card-text>
+    </v-card>
+</template>
