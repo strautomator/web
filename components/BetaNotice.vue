0859b1ef133837e20c81aa1fe34f076294e5695a
Igor Ramadas <igor@devv.com>
2023-02-27 16:51:08 +0100

    Added detail about beta users deletion after some days.

--- components/BetaNotice.vue
+++ components/BetaNotice.vue
@@ -3,6 +3,7 @@
         <v-card-text class="pa-4 text-left">
             <ul class="pl-3">
                 <li>Only available to PRO users.</li>
+                <li>Unused beta accounts will be deleted regularly.</li>
                 <li>Not possible (and not actually needed) to subscribe to PRO via PayPal.</li>
                 <li>Beta profiles and configurations are independent and will not affect the production accounts.</li>
                 <li>The Spotify integration is restricted, please get in touch if you want to use it in Beta.</li>
