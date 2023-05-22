29e4035dc3bc654b0ac2b493e2df69750ccbdc8c
Igor Ramadas <igor@devv.com>
2023-05-22 16:06:41 +0200

    Log request method on failures.

--- src/routes/global.ts
+++ src/routes/global.ts
@@ -4,7 +4,7 @@ const logger = require("anyhow")
 
 export default function (req, res, next) {
     if (res.statusCode >= 400) {
-        logger.error("Routes", req.originalUrl, `Status ${res.statusCode}`, `From ${req.ip} ${req.userId || ""}`)
+        logger.error("Routes", req.method, req.originalUrl, `Status ${res.statusCode || "not sent"}`, `From ${req.ip}`)
     }
 
     next()
