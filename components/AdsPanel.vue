7077858517b5922c051709bfa5e6fa7434bb2c55
Igor Ramadas <igor@devv.com>
2023-11-11 20:59:40 +0100

    Fixed ads.

--- components/AdsPanel.vue
+++ components/AdsPanel.vue
@@ -5,7 +5,7 @@
             <v-card-text class="grey lighten-2">
                 <v-row>
                     <v-col cols="4" v-for="link in links" :key="`affiliate-${link.id}`">
-                        <a :href="linkWiggle" :target="link.id" :title="link.title"><v-img :src="'/images/affiliates/' + link.id + '.png'" max-height="58px" class="mt-1" /></a>
+                        <a :href="link.url" :target="link.id" :title="link.title"><v-img :src="'/images/affiliates/' + link.id + '.png'" max-height="58px" class="mt-1" /></a>
                     </v-col>
                 </v-row>
                 <div class="mt-4 caption black--text text-center">Using our affiliate links is a win-win: you get the best deals, and Strautomator gets a small commission to keep the servers running.</div>
@@ -25,8 +25,7 @@ export default {
             {id: "halfords", title: "Halfords", url: "https://links.devv.com/l/halfords", country: ["gb", "ie", "im", "uk"]},
             {id: "ican", title: "ICAN", url: "https://links.devv.com/l/ican"},
             {id: "nextdns", title: "NextDNS", url: "https://links.devv.com/l/nextdns"},
-            {id: "ribble", title: "Ribble", url: "https://links.devv.com/l/ribble", country: ["at", "de", "gb", "ie", "im", "uk"]},
-            {id: "wiggle", title: "Wiggle", url: "https://links.devv.com/l/wiggle", country: ["de", "uk"]}
+            {id: "ribble", title: "Ribble", url: "https://links.devv.com/l/ribble", country: ["at", "de", "gb", "ie", "im", "uk"]}
         ]
 
         const country = this.$store.state.country || "us"
