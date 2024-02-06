c8900b98137f9596974963d8f322bd56c499a912
Igor Ramadas <igor@devv.com>
2024-02-06 23:04:08 +0100

    Initial "Activity debug" view, working but still needs some polishing.

---
+++ pages/activities/debug.vue
@@ -0,0 +1,129 @@
+<template>
+    <v-layout column>
+        <v-container fluid>
+            <h1>Activity debug</h1>
+            <v-card class="mt-6" outlined>
+                <v-card-text class="pb-2 pb-md-0">
+                    <v-container class="ma-0 pa-0" fluid>
+                        <v-row no-gutters>
+                            <v-col cols="12" :sm="12" :md="10">
+                                <v-text-field v-model="activityId" label="Activity ID or URL" :loading="loading" outlined rounded dense></v-text-field>
+                            </v-col>
+                            <v-col class="text-center text-md-right mt-1" cols="12" :sm="12" :md="2">
+                                <v-btn color="primary" class="mt-n4 mt-md-0" @click="getActivity()" :loading="loading" :disabled="activityId.length < 5" rounded>
+                                    <v-icon left>mdi-text-search</v-icon>
+                                    Inspect
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                    <div v-if="activity === false" class="text-center text-md-left mt-4 mt-md-0 pb-md-4">Enter the activity URL or ID above.</div>
+                    <v-alert class="mt-4 mt-md-0" border="top" color="error" v-else-if="syncError">
+                        {{ syncError }}
+                    </v-alert>
+                </v-card-text>
+            </v-card>
+            <template v-if="activity">
+                <v-card class="mt-4" outlined>
+                    <v-card-title class="accent text-center text-md-left nobreak">
+                        {{ activity.name }}
+                    </v-card-title>
+                    <v-card-text>
+                        <div class="mt-4">
+                            <ul class="ml-0 pl-4">
+                                <li v-for="(value, key) in activity">
+                                    <span class="font-weight-bold">{{ key }}</span> -
+                                    {{ value }}
+                                </li>
+                            </ul>
+                        </div>
+                        <div v-if="garminActivity">
+                            <h4>Garmin metadata:</h4>
+                            <ul class="ml-0 pl-4">
+                                <li v-for="(value, key) in garminActivity">
+                                    <span class="font-weight-bold">garmin.{{ key }}</span> -
+                                    {{ value }}
+                                </li>
+                            </ul>
+                        </div>
+                    </v-card-text>
+                </v-card>
+            </template>
+        </v-container>
+    </v-layout>
+</template>
+
+<script>
+import _ from "lodash"
+import userMixin from "~/mixins/userMixin.js"
+import recipeMixin from "~/mixins/recipeMixin.js"
+
+export default {
+    authenticated: true,
+    mixins: [userMixin, recipeMixin],
+    head() {
+        return {
+            title: "Activity debug"
+        }
+    },
+    data() {
+        return {
+            loading: false,
+            activityId: "",
+            activity: false,
+            garminActivity: false,
+            garminError: null,
+            syncError: null
+        }
+    },
+    methods: {
+        async getActivity() {
+            this.activity = null
+            this.garminActivity = null
+            this.garminError = null
+
+            if (isNaN(this.activityId)) {
+                const arrUrl = this.activityId.replace("https://", "").split("/")
+
+                if (arrUrl.length < 3) {
+                    this.syncError = "Invalid activity URL."
+                    return
+                }
+
+                this.activityId = arrUrl[2]
+            }
+
+            if (isNaN(this.activityId)) {
+                this.syncError = "Invalid activity ID."
+                return
+            }
+
+            try {
+                this.loading = true
+                this.syncError = null
+                this.activity = await this.$axios.$get(`/api/strava/${this.user.id}/activities/${this.activityId}/details`)
+
+                if (!this.activity) {
+                    this.syncError = "Activity not available."
+                }
+
+                if (this.activity.device?.includes("Garmin")) {
+                    try {
+                        this.garminActivity = await this.$axios.$post(`/api/garmin/${this.user.id}/match-activity/${this.activityId}`)
+                    } catch (garminEx) {
+                        this.garminError = garminEx.response?.data?.message || garminEx.toString()
+                    }
+                }
+            } catch (ex) {
+                if (ex.response?.status == 404 || ex.message?.includes("Not Found")) {
+                    this.syncError = "Activity not found."
+                } else {
+                    this.syncError = ex.response?.data?.error ? ex.response.data.error : ex.toString()
+                }
+            }
+
+            this.loading = false
+        }
+    }
+}
+</script>
