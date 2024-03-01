1593dd1395851f026215543726b1432e85efc0cf
Igor Ramadas <igor@devv.com>
2024-03-01 23:11:00 +0100

    Conditional ads based on screen size.

--- components/AdsPanel.vue
+++ components/AdsPanel.vue
@@ -1,10 +1,9 @@
 <template>
-    <div class="mt-4 mb-2 text-center" v-if="!hidden">
-        <div class="text-h6">Affiliate links</div>
+    <div class="mt-6 mb-2 text-center" v-if="!hidden">
         <v-card class="mr-3 ml-3" outlined>
             <v-card-text class="grey lighten-2">
                 <v-row>
-                    <v-col cols="4" v-for="link in links" :key="`affiliate-${link.id}`">
+                    <v-col :cols="$breakpoint.mdAndUp ? 4 : 6" v-for="link in links" :key="`affiliate-${link.id}`">
                         <a :href="link.url" :target="link.id" :title="link.title"><v-img :src="'/images/affiliates/' + link.id + '.png'" max-height="58px" class="mt-1" /></a>
                     </v-col>
                 </v-row>
@@ -21,7 +20,7 @@ export default {
     data() {
         const affiliates = [
             {id: "aliexpress", title: "AliExpress", url: "https://links.devv.com/l/aliexpress-cycling-components"},
-            {id: "decathlon", title: "Decathlon", url: "https://links.devv.com/l/decathlon", country: ["at", "de", "it", "uk"]},
+            {id: "decathlon", title: "Decathlon", url: "https://links.devv.com/l/decathlon", country: ["at", "de", "gb", "it", "ie", "uk"]},
             {id: "halfords", title: "Halfords", url: "https://links.devv.com/l/halfords", country: ["gb", "ie", "im", "uk"]},
             {id: "ican", title: "ICAN", url: "https://links.devv.com/l/ican"},
             {id: "nextdns", title: "NextDNS", url: "https://links.devv.com/l/nextdns"},
@@ -32,12 +31,14 @@ export default {
         const links = affiliates.filter((a) => !a.country || a.country.includes(country))
 
         return {
-            links: _.sortBy(_.sampleSize(links, 3), "id")
+            links: _.sortBy(_.sampleSize(links, this.$breakpoint.mdAndUp ? 3 : 2), "id")
         }
     },
     computed: {
         hidden() {
-            return !this.$store.state.beta && this.$store.state.user?.isPro
+            const urls = ["/billing/"]
+            const hidden = urls.find((u) => this.$route?.path.includes(u))
+            return hidden || (!this.$store.state.beta && this.$store.state.user?.isPro)
         }
     }
 }
