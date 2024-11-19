b8710933eceab735e76113c53210902ac5b2248f
Igor Ramadas <igor@devv.com>
2024-11-19 23:10:22 +0100

    Removed AI photos as it could possibly get into issues with the new Strava API policies.

--- src/routes/api/ai.ts
+++ src/routes/api/ai.ts
@@ -1,15 +1,12 @@
 // Strautomator API: AI
 
-import {ai, strava, users, weather, UserData} from "strautomator-core"
-import {AxiosRequestConfig} from "axios"
+import {ai, weather, UserData} from "strautomator-core"
 import auth from "../auth"
 import _ from "lodash"
 import express = require("express")
 import logger from "anyhow"
 import webserver = require("../../webserver")
 const router: express.Router = express.Router()
-const settings = require("setmeup").settings
-const axios = require("axios").default
 
 // AI
 // --------------------------------------------------------------------------
@@ -49,60 +46,6 @@ router.post("/:userId/activity-generate", async (req: express.Request, res: expr
         webserver.renderJson(req, res, {name, description})
     } catch (ex) {
         webserver.renderError(req, res, ex, 400)
-    }
-})
-
-/**
- * Check if the user has reached the quota for generating images.
- */
-router.get("/:userId/image-quota", async (req: express.Request, res: express.Response) => {
-    try {
-        const user: UserData = (await auth.requestValidator(req, res)) as UserData
-        if (!user) return
-
-        const max = user.isPro ? settings.plans.pro.generatedImages.perWeek : settings.plans.free.generatedImages.perWeek
-        const count = (await ai.getCachedResponses(user, "image", true)) as number
-        webserver.renderJson(req, res, {quota: max - count})
-    } catch (ex) {
-        webserver.renderError(req, res, ex, 404)
-    }
-})
-
-/**
- * Get an AI generated image for the specified activity.
- */
-router.get("/:userId/:urlToken/activity/:activityId.png", async (req: express.Request, res: express.Response) => {
-    try {
-        const user = await users.getById(req.params.userId)
-        if (user.urlToken != req.params.urlToken) throw new Error(`Image not found`)
-
-        // Generate the image.
-        const provider = (req.query.provider as any) || null
-        const style = (req.query.style as any) || null
-        const activity = await strava.activities.getActivity(user, req.params.activityId)
-        const activityWeather = await weather.getActivityWeather(user, activity, true)
-        const aiResponse = await ai.generateActivityImage(user, {activity, activityWeather, provider: provider, style: style})
-
-        if (aiResponse?.response) {
-            if (_.isString(aiResponse.response)) {
-                const options: AxiosRequestConfig = {
-                    method: "GET",
-                    responseType: "stream",
-                    url: aiResponse.response as string
-                }
-
-                axios(options).then((response) => response.data.pipe(res))
-            } else {
-                res.contentType("image/png")
-                res.send(aiResponse.response)
-            }
-        } else if (aiResponse?.rateLimited) {
-            res.status(429).send("Daily limit reached")
-        } else {
-            res.status(404).send("Failed to generate image")
-        }
-    } catch (ex) {
-        webserver.renderError(req, res, ex, 404)
     }
 })
 
