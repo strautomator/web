181f3a836450354126c5f1ac0b20817f10fa550c
Igor Ramadas <igor@devv.com>
2025-01-13 20:51:00 +0100

    Experimenting with chatbase (bot on the Help section).

--- pages/help/index.vue
+++ pages/help/index.vue
@@ -41,6 +41,38 @@
 
                 <feature-links />
             </div>
+            <script>
+                ;(function () {
+                    if (!window.chatbase || window.chatbase("getState") !== "initialized") {
+                        window.chatbase = (...arguments) => {
+                            if (!window.chatbase.q) {
+                                window.chatbase.q = []
+                            }
+                            window.chatbase.q.push(arguments)
+                        }
+                        window.chatbase = new Proxy(window.chatbase, {
+                            get(target, prop) {
+                                if (prop === "q") {
+                                    return target.q
+                                }
+                                return (...args) => target(prop, ...args)
+                            }
+                        })
+                    }
+                    const onLoad = function () {
+                        const script = document.createElement("script")
+                        script.src = "https://www.chatbase.co/embed.min.js"
+                        script.id = "neTz5lyyofpBTh8BNF1kf"
+                        script.domain = "www.chatbase.co"
+                        document.body.appendChild(script)
+                    }
+                    if (document.readyState === "complete") {
+                        onLoad()
+                    } else {
+                        window.addEventListener("load", onLoad)
+                    }
+                })()
+            </script>
         </v-container>
     </v-layout>
 </template>
