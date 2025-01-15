12ba6437b8f1b3e72d4468876375188298c0a044
Igor Ramadas <igor@devv.com>
2025-01-15 22:45:46 +0100

    Moved the AI chat bot startup to the mounted() hook.

--- pages/help/index.vue
+++ pages/help/index.vue
@@ -41,43 +41,6 @@
 
                 <feature-links />
             </div>
-            <script>
-                ;(function () {
-                    if (!window.chatbase || window.chatbase("getState") !== "initialized") {
-                        window.chatbase = (...arguments) => {
-                            if (!window.chatbase.q) {
-                                window.chatbase.q = []
-                            }
-                            window.chatbase.q.push(arguments)
-                        }
-                        window.chatbase = new Proxy(window.chatbase, {
-                            get(target, prop) {
-                                if (prop === "q") {
-                                    return target.q
-                                }
-                                return (...args) => target(prop, ...args)
-                            }
-                        })
-
-                        const onLoad = function () {
-                            const script = document.createElement("script")
-                            script.src = "https://www.chatbase.co/embed.min.js"
-                            script.id = "neTz5lyyofpBTh8BNF1kf"
-                            script.domain = "www.chatbase.co"
-                            document.body.appendChild(script)
-                        }
-                        if (document.readyState === "complete") {
-                            onLoad()
-                        } else {
-                            window.addEventListener("load", onLoad)
-                        }
-                    } else {
-                        document.getElementById("chatbase-bubble-window").style.visibility = "visible"
-                        document.getElementById("chatbase-bubble-button").style.visibility = "visible"
-                        document.getElementById("chatbase-message-bubbles").style.visibility = "visible"
-                    }
-                })()
-            </script>
         </v-container>
     </v-layout>
 </template>
@@ -208,6 +171,40 @@ export default {
 
         if (parent && parent.$data.activeNavBtn) {
             parent.$data.activeNavBtn = "/help"
+        }
+
+        if (!window.chatbase || window.chatbase("getState") !== "initialized") {
+            window.chatbase = (...args) => {
+                if (!window.chatbase.q) {
+                    window.chatbase.q = []
+                }
+                window.chatbase.q.push(args)
+            }
+            window.chatbase = new Proxy(window.chatbase, {
+                get(target, prop) {
+                    if (prop === "q") {
+                        return target.q
+                    }
+                    return (...args) => target(prop, ...args)
+                }
+            })
+
+            const onLoad = function () {
+                const script = document.createElement("script")
+                script.src = "https://www.chatbase.co/embed.min.js"
+                script.id = "neTz5lyyofpBTh8BNF1kf"
+                script.domain = "www.chatbase.co"
+                document.body.appendChild(script)
+            }
+            if (document.readyState === "complete") {
+                onLoad()
+            } else {
+                window.addEventListener("load", onLoad)
+            }
+        } else {
+            document.getElementById("chatbase-bubble-window").style.visibility = "visible"
+            document.getElementById("chatbase-bubble-button").style.visibility = "visible"
+            document.getElementById("chatbase-message-bubbles").style.visibility = "visible"
         }
     },
     beforeDestroy() {
