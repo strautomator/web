32d027658aefc2c88f84c1d5575245d7e06fec54
Igor Ramadas <igor@devv.com>
2025-05-23 19:33:32 +0200

    Updated TC.

--- pages/tc.vue
+++ pages/tc.vue
@@ -10,10 +10,11 @@
 
                 <v-card color="black" class="mb-2 text-left">
                     <v-card-text>
-                        <h4 class="mt-2 mb-6">Policy effective as of October 10th, 2024</h4>
+                        <h4 class="mt-2 mb-6">Policy effective as of May 24th, 2025</h4>
                         <h2 class="mt-2 mb-4">About Strautomator</h2>
                         <p>
-                            Please read these Terms of Service ("Terms", "Terms of Service") carefully before using the Strautomator website (the "Service"), available at https://strautomator.com, and operated by Igor Ramadas ("us", "we", or "our").
+                            Please read these Terms of Service ("Terms", "Terms of Service") carefully before using the Strautomator website (the "Service"), available at https://strautomator.com, and operated by Igor Ramadas Leite Nogueira ("us",
+                            "we", or "our").
                         </p>
                         <p>
                             Your access to and use of the Service is conditioned upon your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who wish to access or use the Service. By accessing or using
@@ -25,15 +26,15 @@
 
                         <h3 class="mt-6 mb-4">Information we collect</h3>
                         <p>
-                            Information we collect includes both information you knowingly and actively provide us when using the Service, and any information automatically sent by your devices in the course of accessing the Service.We only ask for
-                            personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.
+                            Information we collect includes both information you knowingly and actively provide us when using the Service, and any information automatically sent by your devices in the course of accessing the Service. We only ask for
+                            personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we're collecting it and how it will be used.
                         </p>
 
                         <h3 class="mt-1 mb-2">Personal information</h3>
-                        <p>We may ask for personal information, or get information provided by 3rd party integrations, which may include one or more of the following:</p>
+                        <p>We may ask for personal information, or get information provided by third party integrations, which may include one or more of the following:</p>
                         <ul class="mb-6">
+                            <li>Social media profile from Strava (required)</li>
                             <li>Email address</li>
-                            <li>Social media profile from Strava</li>
                             <li>Addresses used in automations</li>
                             <li>Activity metadata from Strava</li>
                             <li>Activity metadata from Garmin</li>
@@ -58,10 +59,10 @@
                             we will delete it within 30 days. Information stored in the server's access log cannot be removed on-demand, but it will be automatically discarded after around 90 days.
                         </p>
 
-                        <h3 class="mt-6 mb-4">Disclosure of information to 3rd parties</h3>
-                        <p>We do not disclose any of your personal information to third parties or individuals, except when required by the EU law.</p>
-                        <p>If you wish to use AI-based features, some of your activity metadata will be sent out to an external AI provider (Anthropic, Google or OpenAI) for processing.</p>
+                        <h3 class="mt-6 mb-4">Disclosure of information to third parties</h3>
+                        <p>We do not disclose any of your personal information to unauthorized third parties or individuals, except when required by the EU law.</p>
                         <p>If you subscribe to Strautomator PRO, some of your personal details will be exchanged with our Merchant of Record, Paddle.com.</p>
+                        <p>If you wish to use AI features, some of your activity metadata will be sent out to an external AI provider (Anthropic, Google, Mistral, OpenAI, xAI) for processing.</p>
 
                         <h3 class="mt-6 mb-4">Controlling your personal information</h3>
                         <p>You always retain the right to withhold personal information from us, with the understanding that your experience of the Service may be affected.</p>
@@ -76,7 +77,7 @@
 
                         <h4 class="mt-1 mb-2">Feature: "Privacy mode"</h4>
                         <p>
-                            You have the option on your account preferences to enable a feature called "Privacy mode". If such feature is enabled, Strautomator will anonymize some of your account details and delete some of the processed data related
+                            You have the option on your account preferences to enable a feature called "Privacy mode". If such feature is enabled, Strautomator will anonymize some of your account details and delete most of the processed data related
                             to your account, including but not limited to:
                         </p>
                         <ul class="mb-6">
@@ -84,66 +85,82 @@
                             <li>Dates when you changed GearWear components</li>
                             <li>Processed activity dates and sport details</li>
                         </ul>
-                        <p>Additionally, some of the Service features will be disabled, including:</p>
+                        <p>Additionally, some of the Service features will be disabled:</p>
                         <ul class="mb-6">
                             <li>Processing of activity data via external AI providers</li>
                             <li>Tracking of personal activity records</li>
                             <li>History log for GearWear components</li>
                         </ul>
                         <p>
-                            Although the "Privacy mode" will decrease our knowledge about your personal and activity data being transmitted to use by Strava, we confirm that it doesn't fully anonymize your details, and it is still possible to link
-                            the information stored by the Service with your account on Strava.
+                            Although the "Privacy mode" will decrease our knowledge about your personal and activity data being transmitted to use by Strava, it doesn't fully anonymize your details, and it is still possible to link the information
+                            stored by the Service with your account on Strava based on its unique ID.
                         </p>
 
                         <h2 class="mt-6 mb-4">Subscriptions</h2>
                         <p>
-                            Some of our services are available only via a subscription plan named Strautomator PRO (the "Subscription"), billed on a monthly or yearly basis, or bought with a single lifetime payment. The Subscription might also be
-                            unlocked via affiliate campaigns.
+                            Some of our services are available only via a subscription plan named Strautomator PRO (the "Subscription"), billed on a monthly or yearly basis, or bought with a single payment that is valid for the lifetime of the
+                            Service. The Subscription might also be acquired via affiliate campaigns.
                         </p>
-                        <p>The monthly or yearly price of the Subscription varies per country and date. The accepted payment currencies are CHF, EUR, GBP and USD. The Subscription will automatically renew at the end of the billing cycle.</p>
-                        <p>Users are free to cancel their existing Subscription at any time, through the "Account" page of the Service. Cancellations are effective immediately, unless stated otherwise during the cancellation process.</p>
+                        <p>
+                            The monthly or yearly price of the Subscription varies per country and date. The accepted payment currencies are CHF, EUR, GBP and USD. The Subscription will automatically renew at the end of the billing cycle. Users will
+                            be reminded of the Subscription renewal on a scheduled basis.
+                        </p>
+
+                        <h3 class="mt-4 mb-2">Merchant of Record</h3>
+                        <p>The online reseller Paddle.com, registered as "Paddle.com Market Ltd.", acts as our Merchant of Record for subscriptions starting from September 14th, 2024.</p>
+
+                        <h3 class="mt-4 mb-2">Payment processing</h3>
+                        <p>
+                            All new Subscription payments are handled by our Merchant of Record, "Paddle.com Market Ltd.". Users that subscribed prior to September 14th, 2024, are billed by "PayPal (Europe) S.à r.l. et Cie, S.C.A.". Payments for
+                            subscriptions made in the form of a Sponsorship via GitHub.com are handled by "Stripe, Inc".
+                        </p>
+                        <p>
+                            We do not collect, process or store credit cards, debit cards, bank details, or other sensitive payment details. All the information related to payment are strictly stored and processed by our payment providers listed
+                            above.
+                        </p>
+                        <p>Questions about payment details should be sent directly to the payment provider, using their contact details that you received via email when the Subscription was made.</p>
 
                         <h3 class="mt-4 mb-2">Fee changes</h3>
-                        <p>At our discretion, we might change the price of the Subscription. Existing subscribers might be affected by the new price, but not before the end of their current billing cycle.</p>
-                        <p>Users that have purchased a lifetime subscription are not affected by price changes, and will be able to continue using all the features that were available at the time of the payment.</p>
+                        <p>
+                            At our discretion, we might change the price of the Subscription. Existing subscribers might be affected by the new price, but not before the end of their current billing cycle. We will try, however, to honor existing
+                            prices so existing subscriptions are not affected by price changes, whenever possible.
+                        </p>
+                        <p>
+                            Users that have purchased a lifetime subscription are not affected by price changes, and will be able to continue using all the features that were available at the time of the payment for a long as the Service is provided.
+                        </p>
 
                         <h3 class="mt-4 mb-2">Updates to the Subscription</h3>
                         <p>
                             New features and integration with new services are not guaranteed in the future. Features that are currently available to Free accounts, might be removed and restricted to users paying for the Subscription. We do not
                             guarantee that existing features of the Subscription will be available in the future. We do, however, remove features only when strictly necessary, for example due to requests made by Strava, or due to legal reasons.
                         </p>
-                        <p>We might release special features that require an additional one-time payment in the future.</p>
+                        <p>We might release special features that require additional payments in the future.</p>
+
+                        <h3 class="mt-4 mb-2">Cancellation of the Subscription</h3>
+                        <p>
+                            Users are free to cancel their existing Subscription at any time, through the "Account" page of the Service, or by cancelling the payment for the next billing cycle directly on the payment platform (Paddle.com, PayPal, or
+                            GitHub Sponsors). Cancellations are effective immediately, unless stated otherwise during the cancellation process.
+                        </p>
 
                         <h3 class="mt-4 mb-2">Suspension of the Subscription</h3>
                         <p>
                             We reserve the right to suspend and cancel any existing Subscription, if we find that it is not being used in accordance with the Terms and Conditions of the Service, or due to legal reasons and sanctions. This applies to
-                            all subscriptions, regardless of their payment date or payment method.
+                            all subscriptions, regardless of their payment date or payment method. In case of cancellation, the subscriber will be refunded with a partial amount equivalent to the remaining days of the current billing cycle, minus the
+                            payment processing fees.
                         </p>
-
-                        <h3 class="mt-4 mb-2">Payment processing</h3>
-                        <p>
-                            Subscription payments are handled by our Merchant of Record, "Paddle.com Market Ltd.". Users that purchased a paid Subscription prior to September 14th, 2024, are billed by "PayPal (Europe) S.à r.l. et Cie, S.C.A.".
-                            Payments for subscriptions made in the form of a Sponsorship via GitHub.com are handled by "Stripe, Inc".
-                        </p>
-                        <p>We do not collect, process or store credit cards, debit cards, bank details, or other sensitive payment details. All the information related to payment are strictly stored and processed by our payment providers.</p>
-                        <p>Questions about payment details should be sent directly to the payment provider, using their contact details that you received via email when the Subscription was made.</p>
-
-                        <h3 class="mt-4 mb-2">Merchant of Record</h3>
-                        <p>The online reseller Paddle.com, registered as "Paddle.com Market Ltd.", is our Merchant of Record.</p>
-                        <p></p>
 
                         <h3 class="mt-4 mb-2">Refunds</h3>
                         <p>
-                            We do not guarantee refunds for any Subscription payments that have already been made. We do not guarantee refunds for subscriptions made by mistake. We might, however, give partial refunds, at our discretion, on a
+                            We do not guarantee refunds for any Subscription payments that have already been made. We do not guarantee refunds for a Subscription made by mistake. We might, however, give partial refunds, at our discretion, on a
                             per-case basis.
                         </p>
-                        <p>It's your responsibility to cancel your Subscription before the next billing cycle, if you do not wish to be charged.</p>
+                        <p>It's your responsibility to cancel your Subscription before the next billing cycle, if you decide that you do not want to keep using the Subscription.</p>
                         <p>
-                            The Service is fully dependent on the Strava API, which is a 3rd party provider. We cannot guarantee that the Strava API will work or exist at any time in the future. If for any reason Strava decides to disable their API,
-                            or in the event that our connection to their API is terminated, all of the core features of the Service will cease to work. In such case, subscribers that had their last payment made in the last 30 days are entitled to a
-                            refund for that last payment alone.
+                            The Service is fully dependent on the Strava API, which is a third party provider. We cannot guarantee that the Strava API will work or exist at any time in the future. If for any reason Strava decides to disable their
+                            API, or in the event that our connection to their API is terminated, all of the core features of the Service will cease to work. In such case, subscribers that had their last payment made in the last 30 days are entitled
+                            to a refund for that last payment alone.
                         </p>
-                        <p>All refunds are processed by our Merchant of Record, Paddle.com.</p>
+                        <p>All refunds are processed by our payment providers directly.</p>
 
                         <h2 class="mt-6 mb-4">Limitation of liability</h2>
                         <p>
@@ -152,7 +169,7 @@
                         </p>
                         <ul class="mb-6">
                             <li>Your access to or use of or inability to access or use the Service</li>
-                            <li>Any conduct or content of any 3rd party on the Service</li>
+                            <li>Any conduct or content of any third party on the Service</li>
                             <li>Any content obtained from the Service</li>
                             <li>Unauthorized access to data transmitted to or from the Service</li>
                         </ul>
@@ -166,7 +183,8 @@
                         <h2 class="mt-6 mb-4">Changes to this policy</h2>
                         <p>
                             At our discretion, we may change our Terms and Conditions to reflect updates to the Service, current acceptable practices, or legislative or regulatory changes. If we decide to change our Terms and Conditions, we will post
-                            the changes here at the same link by which you are accessing now. The full history of changes to our Terms and Conditions are available in our public source control repository, at GitHub.
+                            the changes here at the same link by which you are accessing now. The full history of changes to our Terms and Conditions are available in our public source control repository, at the following address:
+                            https://github.com/strautomator/web/blob/master/pages/tc.vue
                         </p>
 
                         <h2 class="mt-6 mb-4">Contact us</h2>
