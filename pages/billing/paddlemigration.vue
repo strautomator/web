7fdf203840b16cd8bc591d6ad7c576b82a7baffb
Igor Ramadas <igor@ramadas.net>
2025-09-12 21:52:14 +0200

    Updated PayPal to Paddle migration details.

--- pages/billing/paddlemigration.vue
+++ pages/billing/paddlemigration.vue
@@ -23,7 +23,7 @@
                     <v-progress-circular size="32" width="2" v-if="loading" indeterminate></v-progress-circular>
                     <span class="ml-4">Fetching subscription details...</span>
                 </template>
-                <template v-else-if="subscription?.source != 'paypal'">
+                <template v-else-if="subscription?.source == 'paypal'">
                     <p>Your current PRO subscription was not paid via PayPal, so you don't need to migrate.</p>
                     <v-btn color="primary" to="/account" title="Back to my account" exact small rounded outlined nuxt>
                         <v-icon left>mdi-arrow-left</v-icon>
@@ -34,7 +34,8 @@
                 <div v-else-if="migrated">
                     <p class="mt-4 mb-6">The migration process from PayPal to Paddle has finished! Please check your email for the confirmation invoice.</p>
                     <p>
-                        Future payments will now be handled exclusively via Paddle. <span v-if="refundAmount > 0">We have issued a refund of {{ refundAmount }} on your previous PayPal subscription.</span>
+                        Future payments will now be handled exclusively via Paddle.
+                        <span v-if="refundAmount">We have issued a refund of {{ refundAmount }} on your cancelled PayPal subscription.</span>
                     </p>
 
                     <p v-if="errorMessage">{{ errorMessage }}</p>
@@ -49,26 +50,43 @@
                 <div v-else>
                     <p class="mt-4 mb-6">We welcome all users who have subscribed using PayPal to migrate their PRO subscriptions to <a href="https://paddle.com/about" title="Paddle.com" target="paddle">Paddle.com</a>.</p>
                     <h4 class="mb-1 text-md-h6">Why is Strautomator switching to Paddle?</h4>
-                    <p>Paddle is a well established billing platform that supports more payment methods compared to PayPal. Additionally, they act as a Merchant of Record for Strautomator, taking care of all our billing and payment related tasks.</p>
+                    <p>Paddle is a well established billing platform that supports more payment methods compared to PayPal. Additionally, it acts as a Merchant of Record for Strautomator, taking care of all our billing and payment related tasks.</p>
 
                     <h4 class="mb-1 text-md-h6">Do I need to migrate?</h4>
                     <p>No, the PRO subscription migration from PayPal to Paddle is optional.</p>
 
                     <h4 class="mb-1 text-md-h6">What is the migration process?</h4>
                     <p>
-                        First, you'll need to proceed and subscribe again using the new Paddle checkout process. Once your new subscription is activated, your previous PayPal subscription will be automatically cancelled. You'll receive a partial
-                        refund relative to the remaining time of your PayPal subscription.
+                        First, you'll need to proceed and subscribe again using the new Paddle checkout process. Once your new subscription is activated, your previous PayPal subscription will be automatically cancelled. You'll then receive a partial
+                        refund relative to the remaining time of your existing subscription, directly via PayPal.
                     </p>
-                    <v-alert color="accent" v-if="discountId">
-                        As a "thank you" we are offering a discount to a limited number of users that complete the migration to Paddle.<br />The discount code will be automatically applied to the checkout form, and is valid until December 2025.
+                    <p>For the migration you have the option to keep doing yearly payments, or switch to a single-payment lifetime subscription.</p>
+                    <v-alert class="accent" v-if="discountLifetime || discountYearly" outlined>
+                        As a "thank you" we are offering a discount to a limited number of users that migrate from PayPal to Paddle.
+                        <br />
+                        Use code <strong class="primary--text">{{ discountLifetime || "PAYPAL10M" }}</strong> for the lifetime subscription or <strong class="primary--text">{{ discountYearly || "PAYPAL10M" }}</strong> for the yearly subscription at
+                        checkout.
                     </v-alert>
 
-                    <div class="pt-2 text-center text-md-left">
-                        <v-btn color="primary" title="Subscribe via Paddle" @click="paddleCheckout()" :x-large="$breakpoint.mdAndUp" rounded nuxt>
-                            <v-icon left>mdi-database-import-outline</v-icon>
-                            Start migration
-                        </v-btn>
-                    </div>
+                    <v-card outlined>
+                        <v-card-text>
+                            <p>
+                                To start the migration, please select your desired subscription frequency below.<br />
+                                The relevant discount codes, if any, will be applied automatically at checkout.
+                            </p>
+
+                            <div class="pt-2 text-center text-md-left">
+                                <v-btn color="primary" title="Subscribe via Paddle" @click="paddleCheckout(false)" class="mb-4 mb-md-0 mr-md-2" rounded nuxt>
+                                    <v-icon left>mdi-database-import-outline</v-icon>
+                                    Yearly subscription
+                                </v-btn>
+                                <v-btn color="primary" title="Subscribe via Paddle" @click="paddleCheckout(true)" rounded nuxt>
+                                    <v-icon left>mdi-database-import-outline</v-icon>
+                                    Lifetime subscription
+                                </v-btn>
+                            </div>
+                        </v-card-text>
+                    </v-card>
                 </div>
             </div>
         </v-container>
@@ -85,7 +103,7 @@ export default {
     mixins: [subscriptionMixin, userMixin],
     head() {
         return {
-            title: "Billing"
+            title: "Migration to Paddle"
         }
     },
     data() {
@@ -94,8 +112,8 @@ export default {
             subscription: null,
             paddleTransactionId: null,
             migrated: false,
-            lifetime: false,
-            discountId: null,
+            discountLifetime: null,
+            discountYearly: null,
             errorMessage: null,
             refundAmount: 0
         }
@@ -122,17 +140,17 @@ export default {
             }
             window.paddleHasLoaded = true
         }
-        if (this.$route.query.d && this.$route.query.d != "x") {
-            this.discountId = this.$route.query.d
+        if (this.$route.query.dl) {
+            this.discountLifetime = this.$route.query.dl
         }
-        if (this.$route.query.f && this.$route.query.f == "lifetime") {
-            this.lifetime = true
+        if (this.$route.query.dy) {
+            this.discountYearly = this.$route.query.dy
         }
     },
     methods: {
-        async paddleCheckout() {
+        async paddleCheckout(lifetime) {
             try {
-                const priceId = this.lifetime ? this.$store.state.paddle.priceId.lifetime : this.$store.state.paddle.priceId.yearly
+                const priceId = lifetime ? this.$store.state.paddle.priceId.lifetime : this.$store.state.paddle.priceId.yearly
                 const checkout = {
                     settings: {
                         allowLogout: false,
@@ -142,20 +160,19 @@ export default {
                     }
                 }
 
-                if (this.user.paddleTransactionId) {
-                    checkout.transactionId = this.user.paddleTransactionId
-                } else {
-                    checkout.customData = {userId: this.user.id, paypalMigration: this.user.subscriptionId}
-                    checkout.items = [{quantity: 1, priceId: priceId}]
-                    if (this.user.paddleId) {
-                        checkout.customer = {id: this.user.paddleId}
-                    } else if (this.user.email) {
-                        checkout.customer = {email: this.user.email}
-                    }
+                checkout.customData = {userId: this.user.id, paypalMigration: this.subscription.id}
+                checkout.items = [{quantity: 1, priceId: priceId}]
+
+                if (lifetime && this.discountLifetime) {
+                    checkout.discountCode = this.discountLifetime
+                } else if (this.discountYearly) {
+                    checkout.discountCode = this.discountYearly
                 }
 
-                if (this.discountId) {
-                    checkout.discountId = this.discountId
+                if (this.user.paddleId) {
+                    checkout.customer = {id: this.user.paddleId}
+                } else if (this.user.email) {
+                    checkout.customer = {email: this.user.email}
                 }
 
                 Paddle.Checkout.open(checkout)
