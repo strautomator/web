e28cf9a78155e6605c4f18172df3f670f20bc294
Igor Ramadas <igor@ramadas.net>
2025-10-05 21:48:33 +0200

    Updated GDPR download view for medium and large screens.

--- pages/account/download.vue
+++ pages/account/download.vue
@@ -6,36 +6,37 @@
                 <v-card-text class="pa-0">
                     <div class="pa-4">
                         Here you can download a ZIP file with all the data that Strautomator holds about you.
-                        <div v-if="user && user.dateLastArchiveGenerated">You can generate a new archive every {{ this.$store.state.archiveDownloadDays }} days. One was last generated on {{ $dayjs(user.dateLastArchiveGenerated).format("ll") }}.</div>
+                        <div v-if="user && user.dateLastArchiveGenerated">You can generate a new archive every {{ this.$store.state.archiveDownloadDays }} days.</div>
+                        <div v-if="user.dateLastArchiveGenerated">Your last archive was created at {{ $dayjs(user.dateLastArchiveGenerated).format("ll") }}.</div>
                     </div>
                     <h3 class="pl-4 pb-2">What's included?</h3>
                     <ul class="pl-8">
-                        <li class="font-weight-bold gdpr-list-header" v-if="!$breakpoint.mdAndUp">Activities</li>
+                        <li class="font-weight-bold gdpr-list-header" v-if="$breakpoint.mdAndUp">Activities</li>
                         <li>Processed activities</li>
                         <li>Activities queued for processing</li>
-                        <li class="font-weight-bold gdpr-list-header" v-if="!$breakpoint.mdAndUp">FitActivities</li>
+                        <li class="font-weight-bold gdpr-list-header" v-if="$breakpoint.mdAndUp">FitActivities</li>
                         <li>Processed FIT summaries from Garmin</li>
                         <li>Processed FIT summaries from Wahoo</li>
-                        <li class="font-weight-bold gdpr-list-header" v-if="!$breakpoint.mdAndUp">Automations</li>
+                        <li class="font-weight-bold gdpr-list-header" v-if="$breakpoint.mdAndUp">Automations</li>
                         <li>Automation statistics</li>
                         <li>Shared automations</li>
-                        <li class="font-weight-bold gdpr-list-header" v-if="!$breakpoint.mdAndUp">AthleteRecords</li>
+                        <li class="font-weight-bold gdpr-list-header" v-if="$breakpoint.mdAndUp">AthleteRecords</li>
                         <li>Personal activity records</li>
-                        <li class="font-weight-bold gdpr-list-header" v-if="!$breakpoint.mdAndUp">Calendars</li>
+                        <li class="font-weight-bold gdpr-list-header" v-if="$breakpoint.mdAndUp">Calendars</li>
                         <li>Calendar configurations</li>
-                        <li class="font-weight-bold gdpr-list-header" v-if="!$breakpoint.mdAndUp">GearWear</li>
+                        <li class="font-weight-bold gdpr-list-header" v-if="$breakpoint.mdAndUp">GearWear</li>
                         <li>GearWear configurations</li>
                         <li>Battery tracker</li>
-                        <li class="font-weight-bold gdpr-list-header" v-if="!$breakpoint.mdAndUp">Notifications</li>
+                        <li class="font-weight-bold gdpr-list-header" v-if="$breakpoint.mdAndUp">Notifications</li>
                         <li>Read and unread notifications</li>
                         <li>Read announcements</li>
-                        <li class="font-weight-bold gdpr-list-header" v-if="!$breakpoint.mdAndUp">Subscription</li>
+                        <li class="font-weight-bold gdpr-list-header" v-if="$breakpoint.mdAndUp">Subscription</li>
                         <li>PRO subscription details</li>
-                        <li class="font-weight-bold gdpr-list-header" v-if="!$breakpoint.mdAndUp">User</li>
+                        <li class="font-weight-bold gdpr-list-header" v-if="$breakpoint.mdAndUp">User</li>
                         <li>Account details and preferences</li>
                         <li>Strava profile</li>
                         <li>Automation configurations</li>
-                        <li class="font-weight-bold gdpr-list-header" v-if="!$breakpoint.mdAndUp">*.ics</li>
+                        <li class="font-weight-bold gdpr-list-header" v-if="$breakpoint.mdAndUp">*.ics</li>
                         <li>Cached exported calendars</li>
                     </ul>
 
